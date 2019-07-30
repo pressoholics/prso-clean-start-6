3048a60716ab46b07170c549545f2bbc9aa61498
BENJAMIN MOODY <email@benjaminmoody.com>
2019-07-29 20:39:04 -0400

    no message

--- themes/pressotheme-6-child/template_parts/block/example.php
+++ themes/pressotheme-6-child/template_parts/block/example.php
@@ -8,14 +8,6 @@
 // create id attribute for specific styling
 $id = 'block-' . $block['id'];
 ?>
-<div id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $block['className'] ); ?>">
-
-	<?php if( prso_is_gutenberg_editor_request() ): ?>
-
-		<div class="panel">
-			<label>Block Name:</label>
-		</div>
-
-	<?php endif; ?>
+<div>
 
 </div>
