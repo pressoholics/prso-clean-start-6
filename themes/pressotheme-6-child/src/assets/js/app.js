c3a1002d594d36c264ad340b0ad1188a36d4af02
BENJAMIN MOODY <email@benjaminmoody.com>
2019-11-13 19:20:47 -0500

    no message

--- themes/pressotheme-6-child/src/assets/js/app.js
+++ themes/pressotheme-6-child/src/assets/js/app.js
@@ -21,8 +21,17 @@ require('foundation-sites');
 $(document).foundation();
 
 $(document).ready(function() {
+	
+	window.addEventListener('keydown', handleFirstTab);
 
   //Maybe load react scripts
   new PrsoLoadReactScripts();
 
 });
+
+function handleFirstTab(e) {
+  if (e.keyCode === 9) { // the "I am a keyboard user" key
+    document.body.classList.add('user-is-tabbing');
+    window.removeEventListener('keydown', handleFirstTab);
+  }
+}
